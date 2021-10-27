fc2b9e920d653f536c4e0efe47b82026b86d6ef3
Fabian Navarro <info@fabiannavarro.com>
2021-10-27 19:02:23 +0200

    started TweetLoader

--- get_tweets.py
+++ get_tweets.py
@@ -1,19 +1,39 @@
-import twitter
+import tweepy
+import json
 from scrt import constants
 
 class TweetReader():
 
     def __init__(self):
-        api = twitter.Api(consumer_key=constants["API_KEY"],
-                          consumer_secret=constants["API_KEY_SECRET"],
-                          access_token_key=constants["ACCESS_TOKEN"],
-                          access_token_secret=constants["ACCESS_TOKEN_SECRET"])
+        self.auth = tweepy.OAuthHandler(constants["API_KEY"], constants["API_KEY_SECRET"])
+        self.auth.set_access_token(constants["ACCESS_TOKEN"], constants["ACCESS_TOKEN_SECRET"])
+        self.api = tweepy.API(self.auth)
         print("Tweet Reader geladen!")
 
     def load_tweets(self):
-        print("lol")
+        number_of_tweets = 200
+        user_name = "dpa_live"
+        tweets = self.api.user_timeline(screen_name=user_name, tweet_mode = "extended")
+
+        tweet_list = []
+
+        for t in tweets:
+            t = t.full_text.replace("🟠 NEWSBLOG ", "").replace("+++", "").replace("🔵 UND SONST SO? ", "").replace("+ + +", "")
+            tweet_list.append(t)
+
+        return tweet_list
+    
+    def save_tweets(self):
+        data = {}
+        
+        tweets_to_save = self.load_tweets()
+
+        data["tweets"] = [tweets_to_save]
+
+        with open('data/data.json', 'w') as outfile:
+            json.dump(data, outfile)
 
 
 if __name__ == "__main__":
     tr = TweetReader()
-    tr.load_tweets()
+    tr.save_tweets()
