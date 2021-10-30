9493f2c982690c831352a0588bd53f78b865a46e
Fabian Navarro <info@fabiannavarro.com>
2021-10-30 14:14:37 +0200

    started to add audio

--- jokes.py
+++ jokes.py
@@ -1,17 +1,18 @@
 import json
 import random
 import spacy
+import pyttsx3
 from joke_patterns import setups, punchlines
-from get_tweets import TweetReader
+from get_news import NewsReader
 
 class Punchliner():
 
     def __init__(self):
-        tr = TweetReader() #get new tweets and save it at data/data.json
-
+        #tr = TweetReader() #old approach
+        nr = NewsReader() #new
     def get_news(self):
 
-        with open("data/data.json", "r", encoding="utf-8") as file:
+        with open("data/news.json", "r", encoding="utf-8") as file:
             data = json.load(file)
             random_tweet = data[str(random.randint(1, len(data)))]
         
@@ -39,14 +40,19 @@ class Punchliner():
         joke = joke_setup + news
 
         for i in range(punchline_counter):
-            joke = joke + punchlines[punchline_sample[i]].replace("[NN]", joke_object) + " "
+            joke = joke + " " + punchlines[punchline_sample[i]].replace("[NN]", joke_object) + " "
 
         return joke
-
-
+    
+    def make_audio(self):
+        joke = self.make_joke()
+        engine = pyttsx3.init()
+        engine.save_to_file(joke, 'audio/joke.mp3')
+        engine.runAndWait()
+        
 
 if __name__ == "__main__":
     pl = Punchliner()
     print("\n")
-    print(pl.make_joke())
+    print(pl.make_audio())
     print("\n")
